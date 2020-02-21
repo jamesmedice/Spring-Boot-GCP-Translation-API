1cab7771ea35a183d869092b2ab6b5fe2c5135ae
TOSS <tiago.medice@gft.com>
2020-02-21 16:14:12 +0100

    add comment

--- src/main/java/com/medici/app/Application.java
+++ src/main/java/com/medici/app/Application.java
@@ -3,6 +3,11 @@ package com.medici.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * @author a73s
+ *
+ */
 @SpringBootApplication
 public class Application {
 
